1a6b586796d3d5f4965dfb65ae38068e8c2d2b6d
lxdi <alexfordeal@gmail.com>
2026-09-03 22:59:33 +0300

    draft.js init

--- frontend-mindmap/source/components/node-modal.js
+++ frontend-mindmap/source/components/node-modal.js
@@ -2,17 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Modal, Button, Form} from 'react-bootstrap'
 import {registerObject, registerEvent, chkSt, fireEvent, registerReaction} from 'absevents'
+import {Editor, EditorState, RichUtils, convertFromRaw, convertToRaw} from 'draft-js'
+
 
 
 export class NodeModal extends React.Component{
   constructor(props){
     super(props)
 
-    this.state = {isOpen: false}
+    this.state = {isOpen: false, editorState: EditorState.createEmpty()}
     //registerObject('node-modal', {isOpen: false})
-    registerEvent('node-modal', 'open', (stSetter, node) => this.setState({isOpen: true, node: node}))
+    registerEvent('node-modal', 'open', (stSetter, node) => this.setState({isOpen: true, node: node, editorState: deserializeContent(node.note)}))
     registerEvent('node-modal', 'close', (stSetter)=>this.setState({isOpen:false, node: null}))
+
+    this.onChange = editorState => {this.setState({editorState})}
+    this.handleKeyCommand = this.handleKeyCommand.bind(this)
     
+  }
+
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
 
   render(){
@@ -25,7 +46,7 @@ export class NodeModal extends React.Component{
               {getModalBody(this)}
             </div>
             <Modal.Footer>
-              {this.state.isOpen? getFooterButtonsUI(this.state.node):''}
+              {this.state.isOpen? getFooterButtonsUI(this.state.node, this.state.editorState):''}
             </Modal.Footer>
       </Modal>
     )
@@ -41,11 +62,35 @@ const getModalBody = function(comp) {
     return <div>
               <div>
                 <Form.Control type="text" value={comp.state.node.name} onChange={(e)=>changeNameHandler(comp, e)} onFocus={(e) => e.target.select()}/>
-                <Form.Control as="textarea" size='sm' rows={20} value={comp.state.node.note} onChange={(e)=>changeNoteHandler(comp, e)} />
+                <div>
+                  <button onClick={comp._onBoldClick.bind(comp)}>Bold</button>
+                  <Editor editorState={comp.state.editorState} 
+                    onChange={comp.onChange} 
+                    handleKeyCommand={comp.handleKeyCommand}
+                  />
+              </div>
                 <Form.Control placeholder = 'Link' type="text" value={comp.state.node.link} onChange={(e)=>changeLinkHandler(comp, e)}/>
                 <Form.Control placeholder = 'Marker' type="text" value={comp.state.node.marker} onChange={(e)=>changeMarkerHandler(comp, e)}/>
               </div>
           </div>
+}
+
+//                <Form.Control as="textarea" size='sm' rows={20} value={comp.state.node.note} onChange={(e)=>changeNoteHandler(comp, e)} />
+
+const serializeContent = function(editorState) {
+  const contentState = editorState.getCurrentContent();
+  const rawData = convertToRaw(contentState);
+  return JSON.stringify(rawData);
+}
+
+const deserializeContent = function(note) {
+  if (note != null) {
+    const rawData = JSON.parse(note);
+    const contentState = convertFromRaw(rawData);
+    return EditorState.createWithContent(contentState);
+  } else {
+    return EditorState.createEmpty();
+  }
 }
 
 const changeNameHandler = function(comp, e){
@@ -72,11 +117,21 @@ const changeMarkerHandler = function(comp, e){
   fireEvent('state', 'change')
 }
 
-const getFooterButtonsUI = function(node) {
+const getFooterButtonsUI = function(node, editorState) {
     const showDelete = node.left != null && node.right != null
     return <div>
         {showDelete? <Button id='close' onClick={()=>{fireEvent('state', 'panel-remove', [node]); fireEvent('node-modal', 'close')}} variant="danger" style={{marginRight:'5px'}} >Delete</Button>: null}
-        <Button id='close' onClick={()=>fireEvent('node-modal', 'close')} variant="primary">Close</Button>
+        <Button id='close' onClick={()=>closeButtonHandler(node, editorState)} variant="primary">Close</Button>
     </div>
-      
+}
+
+const closeButtonHandler = function(node, editorState) {
+  const initNote = node.note
+  node.note = serializeContent(editorState)
+
+  if (initNote != node.note) {
+    fireEvent('state', 'change')
+  }
+
+  fireEvent('node-modal', 'close')
 }
